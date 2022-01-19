ac8d4c304821904608c6c1923234b8390b608b1d
RonCantWriteCode <707759285@qq.com>
2022-01-19 15:04:27 +0800

    Proxy Pattern

---
+++ src/main/java/cn/roncantwritecode/designpattern/proxypattern/Image.java
@@ -0,0 +1,8 @@
+package cn.roncantwritecode.designpattern.proxypattern;
+
+/**
+ * @author Ron Wu
+ */
+public interface Image {
+    void display();
+}
---
+++ src/main/java/cn/roncantwritecode/designpattern/proxypattern/RealImage.java
@@ -0,0 +1,23 @@
+package cn.roncantwritecode.designpattern.proxypattern;
+
+/**
+ * @author Ron Wu
+ */
+public class RealImage implements Image {
+
+    private String fileName;
+
+    public RealImage(String fileName){
+        this.fileName = fileName;
+        loadFromDisk(fileName);
+    }
+
+    @Override
+    public void display() {
+        System.out.println("Displaying " + fileName);
+    }
+
+    private void loadFromDisk(String fileName){
+        System.out.println("Loading " + fileName);
+    }
+}
