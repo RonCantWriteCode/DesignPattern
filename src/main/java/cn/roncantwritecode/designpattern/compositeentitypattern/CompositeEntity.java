238cbb9d62ee782bbd89cadde6951e19ab75d6ac
RonCantWriteCode <707759285@qq.com>
2022-01-24 09:58:44 +0800

    Composite Entity Pattern

---
+++ src/main/java/cn/roncantwritecode/designpattern/compositeentitypattern/CompositeEntity.java
@@ -0,0 +1,16 @@
+package cn.roncantwritecode.designpattern.compositeentitypattern;
+
+/**
+ * @author Ron Wu
+ */
+public class CompositeEntity {
+    private CoarseGrainedObject cgo = new CoarseGrainedObject();
+
+    public void setData(String data1, String data2){
+        cgo.setData(data1, data2);
+    }
+
+    public String[] getData(){
+        return cgo.getData();
+    }
+}
---
+++ src/main/java/cn/roncantwritecode/designpattern/compositeentitypattern/DependentObject2.java
@@ -0,0 +1,17 @@
+package cn.roncantwritecode.designpattern.compositeentitypattern;
+
+/**
+ * @author Ron Wu
+ */
+public class DependentObject2 {
+
+    private String data;
+
+    public void setData(String data){
+        this.data = data;
+    }
+
+    public String getData(){
+        return data;
+    }
+}
